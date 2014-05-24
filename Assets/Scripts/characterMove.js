5b85b71fc89cedb675ce961acb50add9f15d28ad
Laura Cossette <llcossette@gmail.com>
2014-05-24 15:50:44 -0700

    adds Laura and moveCharacter script

---
+++ Assets/Scripts/characterMove.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var speed : float;
+var gravity : float;
+var controller : CharacterController;
+
+private var moveDirection : Vector3 = Vector3.zero;
+
+function Start () {
+	controller = GetComponent(CharacterController);
+}
+
+function Update () {
+	if(controller.isGrounded){
+		moveDirection = Vector3(Input.GetAxis("Horizontal"),0,0);
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+	}
+	moveDirection.y -= gravity * Time.deltaTime;
+	controller.Move(moveDirection * Time.deltaTime);
+}
